7256ff3711c382817fc3da7c4ff7ea26823f9822
Huy Nguyen <wronghuy@amazon.com>
2022-08-03 09:49:49 -0700

    Added license headers to all py files, and added newlines to all py files
    Signed-off-by: Huy Nguyen <wronghuy@amazon.com>

--- src/playground/sample_data_tooling/sample_data_tests/sample_data_plugin_tests/ad_plugin_data_config_tests/unit-test/average_trend_class_unit_test.py
+++ src/playground/sample_data_tooling/sample_data_tests/sample_data_plugin_tests/ad_plugin_data_config_tests/unit-test/average_trend_class_unit_test.py
@@ -1,3 +1,8 @@
+"""
+Copyright OpenSearch Contributors
+SPDX-License-Identifier: Apache-2.0
+"""
+
 from datetime import datetime
 from json import dumps
 import pytest
--- src/playground/sample_data_tooling/sample_data_tests/sample_data_plugin_tests/ad_plugin_data_config_tests/unit-test/data_trend_interface_unit_test.py
+++ src/playground/sample_data_tooling/sample_data_tests/sample_data_plugin_tests/ad_plugin_data_config_tests/unit-test/data_trend_interface_unit_test.py
@@ -1,3 +1,8 @@
+"""
+Copyright OpenSearch Contributors
+SPDX-License-Identifier: Apache-2.0
+"""
+
 from datetime import datetime
 import pytest
 import sys
